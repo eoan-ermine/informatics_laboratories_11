79c64a3cf6a5c77a00930482347f5bc71cb45ee8
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-26 21:15:45 +0300

    Implement get_article view with corresponding urlpattern

--- blog/urls.py
+++ blog/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.archive),
+    path('article/<int:article_id>', views.get_article, name='get_article'),
 ]
