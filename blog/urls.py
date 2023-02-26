4bb19044f847658b631fc665d980c4b66cd34e27
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-26 21:36:37 +0300

    Implement template for single article, add stylesheets

--- blog/urls.py
+++ blog/urls.py
@@ -20,6 +20,6 @@ from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.archive),
+    path('', views.archive, name="archive"),
     path('article/<int:article_id>', views.get_article, name='get_article'),
 ]
